be57de5b6dd046d3124052a609043c707c6a7ffa
MUSAVVIR1546 <musavvirtak@gmail.com>
2023-12-22 19:10:10 +0530

    Make JS Projects

---
+++ 30 projects/15-Dark mode/Js/script.js
@@ -0,0 +1,10 @@
+let icon = document.getElementById("icon");
+
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "./img/sun.png";
+    } else {
+        icom.src = "img/moon.png"
+    }
+}
